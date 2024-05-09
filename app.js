5f1657ba8d1ea40175d32c813f24a736a71afbfb
Ilhom <ilhom@example.com>
2024-05-09 07:58:37 +0500

    08.05.2024 homework

--- app.js
+++ app.js
@@ -1,159 +1,218 @@
-// // #1
-// let arr = [1,2,3,4]
-// function findAverage(array) {
-//     if (array.length == " "  ) return 0;
-//     let result = 0;
-//     for (let i = 0; i < array.length; i++){
-//         result+= array[i]
+// // 1
+// let str = 'abcd';
+// let arr = [0,3,1,2];
+// function scramble(str, arr) {
+//     let newArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         newArr[arr[i]] = str[i]
 //     }
-//     return result / array.length
+//     return newArr.join('')
 // }
-// let find = findAverage(arr)
-// console.log(find)
+// let scrambl = scramble(str, arr)
+// console.log(scrambl)
 
-// // #2
-// let num = 4;
-// let power = 3;
-// function numberToPower(number, power){
-//     let a = 1
-//     for (let i = 0; i < power; i++) {
-//         a *= number
+// // 2
+// let nums = [5, 0, 3];
+// let str = "The quick brown fox jumps over the lazy dog";
+// function missingWord(nums, str) {
+//     let letters = ''
+//     let num = nums.sort((a, b) => a - b)
+//     str = str.replaceAll(" ", "")
+//     for (let i = 0; i < num.length; i++) {
+//         if (num[i] > str.length) {
+//             return "No mission today"
+//         }
+//         letters += str.charAt(num[i])
 //     }
-//     return a
+//     return letters.toLowerCase()
 // }
-// let powers = numberToPower(num, power)
-// console.log(powers)
+// let word = missingWord(nums, str)
+// console.log(word)
 
-// // #3
-// let num = 6
-// function createArray(number){
-//     let newArray = [];
-//
-//     for(let i = 1; i <= number; i++){
-//         newArray.push(i);
+// // 3
+// let arr = ["1.1","2.2","3.3"]
+// function toNumberArray(stringarray){
+//     let arr = []
+//     for (let i = 0; i < stringarray.length; i++) {
+//         arr.push(+stringarray[i])
 //     }
-//     return newArray;
+//     return arr
 // }
-// let arr = createArray(num)
+// let newArr = toNumberArray(arr)
+// console.log(newArr)
+
+// // 4
+// let text = 'abs'
+// function scrollingText(text){
+//     let arr = []
+//     for (let i = 0; i < text.length; i++) {
+//         arr.push((text.slice(i) + text.slice(0, i)).toUpperCase())
+//     }
+//     return arr
+// }
+// let arr = scrollingText(text)
 // console.log(arr)
 
-// // #4
-// let a = 3
-// let b = 10
-// let c = 12
-// function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-//     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+// // 5
+// let arr = ["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]
+// function twoSort(s) {
+//     let sorted = s.sort()
+//     return sorted[0].split('').join('***')
 // }
-// let goal = goals(a, b, c)
-// console.log(goal)
+// let str = twoSort(arr)
+// console.log(str)
 
-// // #5
-// let f = 1
-// let r = 3
-// function lovefunc(flower1, flower2){
-//     if (flower1 % 2 == 0 && flower2 % 2 == 0) { return false }
-//     if (flower1 % 2 != 0 && flower2 % 2 != 0) { return false }
-//     return true
+// // 6
+// let str = "I love arrays they are my favorite"
+// function stringToArray(string){
+//     return string.split(' ')
 // }
-// let flowers = lovefunc(f, r)
-// console.log(flowers)
+// let arr = stringToArray(str)
+// console.log(arr)
 
-// // #6
-// let h = 1;
-// let m = 1;
-// let s = 1;
-// function past(h, m, s){
-//     let milliseconds = 0
-//     if (h >= 0 && h <= 23){
-//         milliseconds += (h * 3600000)
+// // 7
+// let str = '1,2,3,4,5'
+// function array(string) {
+//     let arr = string.split(',')
+//     if (arr == '' || arr.length < 3) return null
+//     return arr.slice(1, -1).join(' ')
+// }
+// let arr = array(str)
+// console.log(arr)
+
+// // 8
+// let arr = [66, 123, 342]
+// let num = 66
+// function check(a, x) {
+//     return a.includes(x)
+// }
+// let trueFalse = check(arr, num)
+// console.log(trueFalse)
+
+// // 9
+// let arr1 = ['Germany', 'Ukraine'];
+// let arr2 = [2, 0]
+// function uefaEuro2016(teams, scores){
+//     let a = scores[0]
+//     let b = scores[1]
+//     if (a > b) {
+//         return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
 //     }
-//     if (m >= 0 && m <= 59){
-//         milliseconds += (m * 60000)
+//     if (a < b) {
+//         return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
 //     }
-//     if (s >= 0 && s <= 59){
-//         milliseconds += (s * 1000)
+//     if (a = b) {
+//         return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
 //     }
-//     return milliseconds;
 // }
-// let hours = past(h, m, s)
-// console.log(hours)
+// let win = uefaEuro2016(arr1, arr2)
+// console.log(win)
 
-// // #7
-// let num = 17
-// function isPrime(n) {
-//     if (n <= 1)
-//         return false;
-//     for (let i = 2; i < n; i++)
-//         if (n % i == 0)
-//             return false;
-//     return true;
-// }
-// let number = isPrime(num)
-// console.log(number)
-
-// // #8
-// let arr = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]
-// function countPositivesSumNegatives(input) {
-//     if(!input || input == 0) return []
-//     let a = 0;
-//     let b = 0;
-//     for(let i = 0; i < input.length; i++) {
-//         if(input[i] > 0) a++
-//         if (input[i] < 0) {
-//             b += input[i]
+// // 10
+// let arr = [118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]
+// function isVow(array){
+//      let arr = []
+//     let character = ["a", "e", "i", "o", "u"]
+//     for (let i = 0; i < array.length; i++) {
+//         let harp = String.fromCharCode(array[i])
+//         if (character.includes(harp)) {
+//             arr.push(harp)
+//         } else {
+//           arr.push(array[i])
 //         }
 //     }
-//     return [a, b]
+//     return arr
 // }
-// let sums = countPositivesSumNegatives(arr)
-// console.log(sums)
+// let newArr = isVow(arr)
+// console.log(newArr)
 
-// // #9
-// let arr = [1,2,3,4,5]
-// function getAverage(arr){
-//     return arr[Math.trunc(arr.length / 2)]
-// }
-// let middleIndex = getAverage(arr)
-// console.log(middleIndex)
-
-// // #10
-// let str = "Sarah Harry"
-// function abbrevName(name){
-//     let arr = name.split(' ')
-//     let str = []
+// // 11
+// let arr = [70,85,83,32,82,79,72,32,68,65,72]
+// function arrowFunc (arr) {
+//     let str = ''
 //     for (let i = 0; i < arr.length; i++) {
-//         str.push(arr[i].substring(0, 1))
+//         str += String.fromCharCode(arr[i])
 //     }
-//     return str.join('.').toUpperCase()
+//     return str
 // }
-// let nameStr = abbrevName(str)
-// console.log(nameStr)
+// let newArr = arrowFunc(arr)
+// console.log(newArr)
 
-// // #11
-// let a = '123'
-// let b = '321'
-// function sumStrings(a,b) {
-//     let num1 = BigInt(a)
-//     let num2 = BigInt(b)
-//     return String(num1 + num2)
+// // 12
+// let str = "hello"
+// function encode(string) {
+//     let arr = string.split('')
+//     let str = ''
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] == 'a') {
+//             str += 1
+//             continue
+//         }
+//         if (arr[i] == 'e') {
+//             str += 2
+//             continue
+//         }
+//         if (arr[i] == 'i') {
+//             str += 3
+//             continue
+//         }
+//         if (arr[i] == 'o') {
+//             str += 4
+//             continue
+//         }
+//         if (arr[i] == 'u') {
+//             str += 5
+//             continue
+//         }
+//         str += arr[i]
+//     }
+//     return str
 // }
-// let bigIntNumbers = sumStrings(a, b)
-// console.log(bigIntNumbers)
+// function decode(string) {
+//     let arr = string.split('')
+//     let str = ''
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] == '1') {
+//             str += 'a'
+//             continue
+//         }
+//         if (arr[i] == '2') {
+//             str += 'e'
+//             continue
+//         }
+//         if (arr[i] == '3') {
+//             str += 'i'
+//             continue
+//         }
+//         if (arr[i] == '4') {
+//             str += 'o'
+//             continue
+//         }
+//         if (arr[i] == '5') {
+//             str += 'o'
+//             continue
+//         }
+//         str += arr[i]
+//     }
+//     return str
+// }
+// let newStr = encode(str)
+// let newStr2 = decode(newStr)
+// console.log(newStr, newStr2)
 
-// // #12
-// let num = -321
-// function reverseNumber(n) {
-//     let r = n.toString().split('').reverse().join('');
-//     return parseInt(r) * Math.sign(n);
+// // 13
+// let arr = [1, 3, 4, 6]
+// function twosDifference(nums){
+//     nums.sort((a, b) => a - b);
+//
+//     let result = [];
+//
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums.includes(nums[i] + 2)) {
+//             result.push([nums[i], nums[i] + 2]);
+//         }
+//     }
+//     return result;
 // }
-// let negative = reverseNumber(num)
-// console.log(negative)
-
-// // #13
-// let str = "turns out random test cases are easier than writing out basic ones"
-// function findShort(s){
-//     let arr = s.split(' ')
-//     return  Math.min.apply(Math, arr.map(function(str) { return str.length; }))
-// }
-// let shortLength = findShort(str)
-// console.log(shortLength)
+// let newArr = twosDifference(arr)
+// console.log(newArr)
