92d6e4202d8201729f0c5c2a44f30317c418be53
Ilhom <ilhom@example.com>
2024-05-13 14:33:47 +0500

    12.05.2024 homework

--- app.js
+++ app.js
@@ -80,3 +80,65 @@
 // let a = [2,3,4,6,8,7,3,1,5,6,8,9]
 // a = a.find((a) => a > 4)
 // console.log(a)
+
+
+// ixtiyoriy masala
+// // #1
+// function numberProperty(n) {
+//     let arr = []
+//     function isPrime(n) {
+//         if (n <= 1) {
+//             return false;
+//         }
+//         for (let i = 2; i <= Math.sqrt(n); i++) {
+//             if (n % i === 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     let prime = isPrime(n)
+//     arr.push(prime)
+//     let oddEven = n % 2 == 0 ? arr.push(true) : arr.push(false)
+//     let multiple = n % 10 == 0 ? arr.push(true) : arr.push(false)
+//     return arr
+// }
+// let res = numberProperty(5)
+// console.log(res)
+
+// // #2
+// function sortVowels(s) {
+//     if (typeof s !== 'string') return ''
+//     const vowels = 'aeiouAEIOU';
+//     let result = '';
+//
+//     for (let i = 0; i < s.length; i++) {
+//         if (vowels.includes(s[i])) {
+//             result += '|' + s[i] + '\n' ;
+//         } else {
+//             result +=s[i] + '|' + '\n' ;
+//         }
+//     }
+//     return result;
+// }
+//
+// let res = sortVowels("Codewars")
+// console.log(res)
+
+// // #3
+// function validateWord(s){
+//     s = s.toLowerCase();
+//     const charCount = {};
+//     for (let char of s) {
+//         charCount[char] = (charCount[char] || 0) + 1;
+//     }
+//     const firstCharCount = charCount[s[0]];
+//     for (let char in charCount) {
+//         if (charCount[char] !== firstCharCount) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// let res = validateWord("abcabcd")
+// console.log(res)
