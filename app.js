c86885908fef2f33b98244408f4c4aa40af2c3b3
Ilhom <ilhom@example.com>
2024-05-14 21:13:03 +0500

    14.05.2024 homework

--- app.js
+++ app.js
@@ -1,103 +1,78 @@
-// // #1
-// let user = {
-//     id: 1,
-//     name: "Leanne Graham",
-//     username: "Bret",
-//     email: "Sincere@april.biz"
+// # 1
+
+// # 2
+// let str = "1.1.1.1"
+// let arr = str.split(".").join("[.]")
+// console.log(arr)
+
+// # 3
+// let arr = [1, 3, 4, 6]
+// function twosDifference(nums){
+//     nums.sort((a, b) => a - b);
+//
+//     let result = [];
+//
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums.includes(nums[i] + 2)) {
+//             result.push([nums[i], nums[i] + 2]);
+//         }
+//     }
+//     return result;
 // }
-// let result = Object.entries(user);
-// localStorage.setItem("user", JSON.stringify(result))
+// let newArr = twosDifference(arr)
+// console.log(newArr)
 
-// // #2
-// let user = {
-//     id: 1,
-//     name: "Leanne Graham",
-//     username: "Bret",
-//     email: "Sincere@april.biz"
+// # 4
+// let str = "luffy is still joyboy"
+// let arr = str.split(" ").slice(-1)
+// console.log(arr[0].length)
+
+// # 5
+// let str = "red white blue" // "red blue gold"
+// let arr = str.split(" ");
+// const lengths = arr.map(a=> a.length);
+// let maxLength = lengths.lastIndexOf(Math.max(...lengths));
+// console.log(arr[maxLength])
+
+// # 6
+// let str = "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"
+// let arr = str.match(/[0-9]/g )
+// console.log(arr)
+
+// # 7
+// function isItLetter (letter) {
+//     let alphabet = "abcdefghijklmnopqrstuvwxyz"
+//     if (alphabet.includes(letter)) return true
+//     return false
 // }
-// localStorage.setItem("user", JSON.stringify(user))
-// let newname = prompt("Enter your name")
-// let newUserName = JSON.parse(localStorage.getItem("user"))
-// newUserName.name = newname
-// localStorage.setItem("user", JSON.stringify(newUserName))
-//
-// let newusername = prompt("Enter nick name")
-// let newUsername = JSON.parse(localStorage.getItem("user"))
-// newUsername.username = newusername
-// localStorage.setItem("user", JSON.stringify(newUsername))
-//
-// let newemail = prompt("Enter email")
-// let newUserEmail = JSON.parse(localStorage.getItem("user"))
-// newUserEmail.email = newemail
-// localStorage.setItem("user", JSON.stringify(newUserEmail))
+// let res = isItLetter('h')
+// console.log(res)
 
-// // #3
-// let users = [
-//     {
-//         id: 1,
-//         name: "Leanne Graham",
-//         username: "Bret",
-//         email: "Sincere@april.biz"
-//     },
-// ]
+// # 8
+// function isItAnagram(str1, str2) {
+//     let normalized1 = str1.toLowerCase().split('').sort().join('');
+//     let normalized2 = str2.toLowerCase().split('').sort().join('');
 //
-// function addUser() {
-//     const newUser = {
-//         id: users.length + 1,
-//         name: prompt("Enter your name: "),
-//         username: prompt("Enter your nick name: "),
-//         email: prompt("Enter a email: ")
-//     }
-//     users = [...users, newUser]
+//     return normalized1 === normalized2;
 // }
-// addUser()
-// localStorage.setItem("users", JSON.stringify(users))
-// console.log(users)
+// let res = isItAnagram("Listen", "Silent")
+// let res2 = isItAnagram("Apple", "Papel")
+// let res3 = isItAnagram("Rat", "Car")
+// console.log(res);
+// console.log(res2);
+// console.log(res3);
 
-// // #4
-// let users = [
-//     {
-//         id: 1,
-//         name: "Anna",
-//         username: "Anna",
-//         email: "Sincere@april.biz"
-//     },
-//     {
-//         id: 2,
-//         name: "Mark",
-//         username: "Marko",
-//         email: "mark@gmail.com"
-//     }
-// ]
-//
-// localStorage.setItem("user", JSON.stringify(users))
-// let username = prompt("Enter remove user name")
-// let removeUser = JSON.parse(localStorage.getItem("user"))
-// removeUser = removeUser.filter((user) => user.name !== username)
-// localStorage.setItem("user", JSON.stringify(removeUser))
+// # 9
+// let arr = [ 1, 1, 1, 2, 1, 1 ]
+// let newArr = Math.max(... new Set(arr))
+// console.log(newArr)
 
-// // #5
-// let users = [
-//     {
-//         id: 1,
-//         name: "Anna",
-//         username: "Anna",
-//         email: "Sincere@april.biz"
-//     },
-//     {
-//         id: 2,
-//         name: "Mark",
-//         username: "Marko",
-//         email: "mark@gmail.com"
-//     }
-// ]
-// localStorage.setItem("user", JSON.stringify(users))
+// # 10
+// function contrast (word1, word2) {
+//     let str1 = word1.join('');
+//     let str2 = word2.join('');
 //
-// const btn = document.getElementById("btn")
-//
-// btn.addEventListener("click", () => {
-//     const isAgree = confirm("Are you sure to delete all?")
-//     if (isAgree) {
-//         localStorage.clear()
-//     }
-// })
+//     return str1 === str2;
+// }
+// let res = contrast( ["ab", "c"],  ["a", "bc"])
+// console.log(res)
