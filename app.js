cb3c4dd71b3b4e2980552447aedbb2f100832f88
Ilhom <ilhom@example.com>
2024-05-29 14:28:40 +0500

    28.05.2024 homework

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const form = document.querySelector("form")
+// change currency
+const form = document.querySelector("#form")
 const input = document.querySelector("#currency")
 const selectFrom = document.querySelector("#from")
 const selectTo = document.querySelector("#to")
 const div = document.querySelector("#res")
-
+// update currency
+const formChangeCurrency = document.querySelector("#change-currency")
+const selectCurrency = document.querySelector("#select-currency")
+const usd = document.querySelector("#usd")
+const uzs = document.querySelector("#uzs")
+const rub = document.querySelector("#rub")
+const eur = document.querySelector("#eur")
+const res = document.querySelector("#result")
+// currency object
 let currency = {
     USD: {
         USD: 1,
@@ -30,9 +39,10 @@ let currency = {
         EUR: 0.010
     }
 }
-
+// set localstorage
 localStorage.setItem("Rates", JSON.stringify(currency))
 const rates = JSON.parse(localStorage.getItem("Rates"))
+// change currency
 function changeCurrency (e) {
     e.preventDefault()
 
@@ -50,5 +60,32 @@ function changeCurrency (e) {
     `
     form.reset()
 }
+// update currency value
+function updateCurrency (e) {
+    e.preventDefault()
 
+    let usdValue = +usd
+    let uzsValue = +uzs
+    let rubValue = +rub
+    let eurValue = +eur
+
+    rates[selectCurrency]["USD"] = usdValue
+    rates[selectCurrency]["UZS"] = uzsValue
+    rates[selectCurrency]["EUR"] = eurValue
+    rates[selectCurrency]["RUB"] = rubValue
+
+    localStorage.setItem("Rates", JSON.stringify(rates))
+}
+// event forms and inputs
 form.addEventListener("submit", changeCurrency)
+formChangeCurrency.addEventListener("submit", updateCurrency)
+input.addEventListener("input" , () => {
+        input.value = input.value.replace(/-/g, "")})
+usd.addEventListener("input" , () => {
+    usd.value = usd.value.replace(/-/g, "")})
+rub.addEventListener("input" , () => {
+    rub.value = rub.value.replace(/-/g, "")})
+uzs.addEventListener("input" , () => {
+    uzs.value = uzs.value.replace(/-/g, "")})
+eur.addEventListener("input" , () => {
+    eur.value = eur.value.replace(/-/g, "")})
