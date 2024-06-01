5db68d14f45a1305a28455cb8dfc3cfca673197a
Ilhom <ilhom@example.com>
2024-06-01 22:36:03 +0500

    01.06.2024 homework

--- src/app.js
+++ src/app.js
@@ -1,43 +1,27 @@
 const box = document.querySelector("#box")
 
-// const renderData = (d) => {
-//     d.forEach(item => {
-//         const div = document.createElement("div")
-//         div.innerHTML = `
-//             <p>ID ${item.id}</p>
-//             <p>Title ${item.title}</p>
-//             <img class="img" src="${item.images[0]}" alt="">
-//             <p>Des ${item.description}</p>
-//         `
-//         box.appendChild(div)
-//     })
-// }
-//
-//
-// fetch("https://api.escuelajs.co/api/v1/products")
-//     .then(response => response.json())
-//     .then(data => renderData(data))
-
-
 const ren = (d) => {
-    d.data.forEach(item => {
+    d.forEach(item => {
+
+    })
+    for (let i = 0; i < 32; i++) {
         const div = document.createElement("div")
         div.classList.add("card")
         div.innerHTML = `
             <div class="flex items-center justify-center flex-col">
-                <img class="img" src="${item.avatar}" alt="">
+                <img class="img" src="${d[i].category.image}">
                 <div class="title">
-                    <p>${item.last_name}</p>
-                    <p>${item.first_name}</p>
+                    <p>${d[i].title}</p>
+                    <p>$${d[i].price}</p>
                 </div>
-                <p class="text-white">${item.email}</p>
+                <p>${d[i].description}</p>
             </div>
         `
         box.appendChild(div)
-    })
+    }
 }
 
 
-fetch("https://reqres.in/api/users?page=2")
+fetch("https://api.escuelajs.co/api/v1/products")
     .then(res => res.json())
     .then(d => ren(d))
