f79c8841698ff970842119da4f509d95df22e430
Ilhom <ilhom@example.com>
2024-06-05 23:36:06 +0500

    05.06.2024 homework

--- js/app.js
+++ js/app.js
@@ -1,45 +1,22 @@
-const $form = document.querySelector("#search-city-weather")
-const $input = document.querySelector("#input")
-const $weatherStatusImg = document.querySelector("#weather-status-img")
-const $weatherIndicator = document.querySelector("#weather-gradus")
-const $location = document.querySelector("#location")
-const $time = document.querySelector("#time")
-const $sunset = document.querySelector("#sunset")
-const $sunset2 = document.querySelector("#sunset2")
-const $sunrise2 = document.querySelector("#sunrise2")
-const $humidity = document.querySelector("#humidity")
-const $sun = document.querySelector("#sun")
-const $airPressure = document.querySelector("#air-pressure")
+const $imgBox = document.querySelector("#img-box")
+const $changeBtn = document.querySelector("#change-img")
 
-const API_KEY = "644f6ce0ca9e401ebb891832211707"
-const weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const renderData = (d) => {
-    $weatherStatusImg.src = "https:" + d.current.condition.icon
-    $weatherIndicator.innerText = d.current.temp_c + "°"
-    $location.innerText = `${d.location.name}, ${d.location.country}`
-    $time.innerText = `${weekDay[new Date().getDay()]}, ${d.location.localtime}`
-    $sunset.innerText = d.forecast.forecastday[0].astro.sunset
-    $sunrise2.innerText = d.forecast.forecastday[0].astro.sunrise
-    $sunset2.innerText = d.forecast.forecastday[0].astro.sunset
-    $humidity.innerText = d.current.humidity + "%"
-    $sun.innerText = d.current.uv + " out of 10"
-    $airPressure.innerText = d.current.pressure_mb + " Pa"
-}
-
-const data = (city) => {
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=yes&alerts=yes`)
+const fetchInfo = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
         .then(res => res.json())
-        .then(data => renderData(data))
+        .then(data => renderRandomImg(data))
 }
 
-data("Tashkent")
-
-const searchCityWeather = (e) => {
-    e.preventDefault()
-    data($input.value)
-
-    $form.reset()
+const renderRandomImg = (d) => {
+    while ($imgBox.firstChild) {
+        $imgBox.removeChild($imgBox.firstChild)
+    }
+    const img = document.createElement("img")
+    img.src = d.message
+    $imgBox.appendChild(img)
 }
 
-$form.addEventListener("submit", searchCityWeather)
+
+
+$changeBtn.addEventListener("click", fetchInfo)
