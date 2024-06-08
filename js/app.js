53391e490d122896efaf2240d897ac581d70c3d2
Ilhom <ilhom@example.com>
2024-06-08 12:14:23 +0500

    07.06.2024 homework

--- js/app.js
+++ js/app.js
@@ -1,45 +1,36 @@
-const $form = document.querySelector("#search-city-weather")
-const $input = document.querySelector("#input")
-const $weatherStatusImg = document.querySelector("#weather-status-img")
-const $weatherIndicator = document.querySelector("#weather-gradus")
-const $location = document.querySelector("#location")
-const $time = document.querySelector("#time")
-const $sunset = document.querySelector("#sunset")
-const $sunset2 = document.querySelector("#sunset2")
-const $sunrise2 = document.querySelector("#sunrise2")
-const $humidity = document.querySelector("#humidity")
-const $sun = document.querySelector("#sun")
-const $airPressure = document.querySelector("#air-pressure")
+const $result = document.querySelector("#result")
 
-const API_KEY = "644f6ce0ca9e401ebb891832211707"
-const weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const renderData = (d) => {
-    $weatherStatusImg.src = "https:" + d.current.condition.icon
-    $weatherIndicator.innerText = d.current.temp_c + "°"
-    $location.innerText = `${d.location.name}, ${d.location.country}`
-    $time.innerText = `${weekDay[new Date().getDay()]}, ${d.location.localtime}`
-    $sunset.innerText = d.forecast.forecastday[0].astro.sunset
-    $sunrise2.innerText = d.forecast.forecastday[0].astro.sunrise
-    $sunset2.innerText = d.forecast.forecastday[0].astro.sunset
-    $humidity.innerText = d.current.humidity + "%"
-    $sun.innerText = d.current.uv + " out of 10"
-    $airPressure.innerText = d.current.pressure_mb + " Pa"
+
+
+
+
+
+const renderApiHtml = (data) => {
+    console.log(data)
+    const $fragment = document.createDocumentFragment()
+
+    data.forEach(item => {
+        const $div = document.createElement("div")
+        $div.className = "card"
+        $div.innerHTML = `
+            <img src="${item.image}" />
+            <p>${item.title}</p>
+            <span>${item.discount}</span>
+            <span>${item.price}</span>
+            <p>${item.description}</p>
+        `
+        $fragment.appendChild($div)
+    })
+    $result.appendChild($fragment)
 }
 
-const data = (city) => {
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=yes&alerts=yes`)
+
+const renderApi = () => {
+
+    fetch("https://6662ac4162966e20ef097175.mockapi.io/api/products/products")
         .then(res => res.json())
-        .then(data => renderData(data))
+        .then(data => renderApiHtml(data))
 }
 
-data("Tashkent")
-
-const searchCityWeather = (e) => {
-    e.preventDefault()
-    data($input.value)
-
-    $form.reset()
-}
-
-$form.addEventListener("submit", searchCityWeather)
+renderApi()
